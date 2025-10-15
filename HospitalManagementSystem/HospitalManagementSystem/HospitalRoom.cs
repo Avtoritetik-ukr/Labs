0a7a776b6e5d868af0821f9328c105c06945c4b2
Avtoritetik-ukr <mkarmaza184@gmail.com>
2025-10-15 16:54:22 +0300

    add

---
+++ HospitalManagementSystem/HospitalManagementSystem/HospitalRoom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem
+{
+    public class HospitalRoom
+    {
+        public int RoomNumber { get; set; }
+        public int Capacity { get; private set; } 
+        public List<Patient> Patients { get; } = new List<Patient>();
+        public HospitalRoom(int roomNumber, int capacity)
+        {
+            RoomNumber = roomNumber;
+            Capacity = capacity;
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            if (Patients.Count < Capacity)
+            {
+                Patients.Add(patient);
+                Console.WriteLine($"Пацієнт {patient.Name} доданий у палату {RoomNumber}\n");
+            }
+            else
+            {
+                Console.WriteLine($"Палата {RoomNumber} переповнена! Неможливо додати пацієнта.\n");
+            }
+        }
+    }
+}
